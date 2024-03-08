82c39e8939ce1522dad82fe66475f9eac443300b
brunolimabh <bruno.lima@sptech.school>
2024-03-08 13:26:21 -0300

    fix: corrigindo pasta "build"

--- cfb/build/aula02.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const vnome = "Bruno";
-const vsobrenome = "Henrique";
-const vcurso = "cco";
-console.log(vnome);
-console.log(vsobrenome);
--- cfb/build/aula04.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-let nome;
-nome = "Bruno";
-let valor = 0;
-valor = 10.5;
-console.log(valor);
