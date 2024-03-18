3bcdf169fdb31c0e713680a3274e1b1a87a3b37f
brunolimabh <bruno.lima@sptech.school>
2024-03-18 10:21:02 -0300

    feat: add express

--- felipe-rocha/src/index.ts
+++ felipe-rocha/src/index.ts
@@ -1,1 +1,14 @@
-console.log("ola")
+import exprees from "express";
+import { config } from "dotenv";
+
+config();
+
+const app = exprees();
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
